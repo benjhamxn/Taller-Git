c4204a1e38b78ed12f7a27a94cd688f1ac967a15
Benjamin Rios <griosa@est.ups.edu.ec>
2020-12-15 14:33:33 -0500

    Agregado datos del proyecto

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 # Autor: Benjamin RIos <griosa@est.ups.edu.ec>
 # Fecha: 15-12-2020
 def main ():
-    print( "Hola Mundo!" )
+    nombre = input("Hola, como te llamas?: ")
+    print( "Hola", nombre, "Bienvenido a Git!" )
 if __name__ == "__main__" :
     main()
